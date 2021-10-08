bbd2e73e8f6443e130eec2cc4b2366e3663c3610
NeoGlanding <neoglanding@protonmail.com>
2021-10-08 21:49:29 +0700

    done backend frontend profile

--- Client/src/components/Profile.jsx
+++ Client/src/components/Profile.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import axios from 'axios'
 
 const Profile = props => {
+    const [info, setInfo] = useState({})
 
-    const getProfile = token => {
-        axios.post()
+    const getProfile = async token => {
+        try {
+            const data = await axios.get('http://localhost:3001/users', {
+                headers: {
+                    token
+                }
+            });
+            setInfo(data.data)
+        } catch(err) {
+            console.log(err)
+        }
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        getProfile(token);
+    }, [])
+
+    useEffect(() => {
+        console.log(info)
+    }, [info])
 
 
     return (
-        <div>
-            <h1>Name</h1>
-            <p>Summary</p>
-            <p>Email</p>
-        </div>
+        <React.Fragment>
+            {
+                !info.name ? 
+                null :
+                <div>
+                    <h1>{info.name}</h1>
+                    <p>{info.jobTitle}</p>
+                    <p>{info.summary}</p>
+                    <p>{info.email}</p>
+                </div>
+            } 
+        </React.Fragment>
     )
 }
 
--- Server/controllers/User.js
+++ Server/controllers/User.js
@@ -18,7 +18,7 @@ class Users {
                         message: 'Error! Either email or Password Wrong'
                     })  
                 } else {
-                    const token = jwt.sign({name:exist.name, email: exist.email}, 'secret-key');
+                    const token = jwt.sign({name:exist.name, email: exist.email, role: exist.role, id: exist._id}, 'secret-key');
                     res.status(200).json({
                         token
                     })
@@ -29,13 +29,14 @@ class Users {
             console.log(error)
         }
     }
-    // static getProfile = async (req, res, next) => {
-    //    try {
-    //     const {token}
-    //    } catch(err) {
-    //        console.log(err)
-    //    }
-    // }
+    static getProfile = async (req, res, next) => {
+       try {
+            const own = await User.findOne({email: req.user.email}).select('-password -__v');
+            res.status(200).json(own)
+       } catch(err) {
+           console.log(err)
+       }
+    }
 }
 
 module.exports = Users
