b3d51e036678b3d6c7c2ec136efa72b13bf031aa
NeoGlanding <neoglanding@protonmail.com>
2021-10-11 13:26:18 +0700

    done

--- Client/src/App.js
+++ Client/src/App.js
@@ -9,6 +9,7 @@ import PageHome from "./page/PageHome";
 import PageLogin from "./page/PageLogin";
 import PublicRoute from "./routes/PublicRoute";
 import PrivateRoute from "./routes/PrivateRoute";
+import HRoutes from './routes/HrOnlyRoutes'
 import PageProfile from "./page/PageProfile";
 import PageRegis from "./page/PageRegis";
 import PageForm from "./page/PageForm";
@@ -31,7 +32,7 @@ function App() {
       <NavG />
       <Switch>
         <Route exact path="/" component={PageHome} />
-        <Route exact path="/postJob" component={PageForm} />
+        <HRoutes exact path="/postjob" component={PageForm} />
         <PublicRoute exact path="/login" component={PageLogin} />
         <PublicRoute exact path="/register" component={PageRegis} />
         <PrivateRoute exact path="/profile" component={PageProfile} />
