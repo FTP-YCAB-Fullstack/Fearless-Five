5d5116ef88c5ac2aaaef888d4a9848f7cbc934ac
TGB <087762156347.ky@gmail.com>
2021-10-11 14:31:33 +0800

    setUp list job

---
+++ Client/src/components/CardList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CardList = () =>{
+    return(
+        <div>
+            <span>
+                job list
+            </span>
+        </div>
+    )
+}
+
+export default CardList
---
+++ Client/src/page/PageListJob.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardList from "../components/cardList";
+
+const PageListJob = () =>{
+    return(
+        <div>
+            <CardList/>
+        </div>
+    )
+}
+
+export default PageListJob
