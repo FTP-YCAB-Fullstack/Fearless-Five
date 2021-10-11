cb143f1e71db3ec0e49bb938344c06ed7000c1fa
GilangSebastian <gilangsm2024@gmail.com>
2021-10-11 13:59:25 +0700

    add apply router into main router

--- Server/routers/index.js
+++ Server/routers/index.js
@@ -4,10 +4,12 @@ const authentication = require('./authentication');
 const users = require('./userRoutes');
 const perusahaan = require('./companyRoutes');
 const vacancy = require('./vacancyRoutes')
+const apply = require('./applyRoutes');
 
 router.use(authentication)
 router.use(users)
 router.use(perusahaan)
 router.use(vacancy)
+router.use(apply)
 
 module.exports = router;
