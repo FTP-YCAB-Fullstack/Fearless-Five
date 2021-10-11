5a78222812fb1bd63e0df174a237b52f29aabce7
TGB <087762156347.ky@gmail.com>
2021-10-11 15:32:20 +0800

    fetch api list

--- Client/src/page/PageListJob.jsx
+++ Client/src/page/PageListJob.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "../components/CardList";
+import axios from "axios";
 
-const PageListJob = () =>{
-    return(
-        <div>
-            <CardList/>
-        </div>
-    )
-}
+const PageListJob = () => {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:3001/vacancies", {
+        headers: {
+          token,
+        },
+      })
+      .then((res) => setJobs(res.data));
+  }, []);
 
-export default PageListJob
+  return (
+    <div className="flex justify-center gap-x-6">
+      {jobs.map((el, index) => (
+        <CardList {...el} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default PageListJob;
