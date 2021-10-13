170c75a87288ef957905bce3a9fe777399569b97
GilangSebastian <gilangsm2024@gmail.com>
2021-10-14 01:07:03 +0700

    styling navbar 50%

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -19,15 +19,17 @@ const Navbar = props => {
     }
 
     return (
-        <nav className="flex flex-row justify-around">
-            <Link to="/">Home</Link>
-            {status === 'hrd' ? <Link to="/postjob">Post Job</Link> : null}
-            <Link to="/jobs">List Job</Link>
-            {!auth ? <Link to="/register">Register</Link> : null}
-            {!auth ? <Link to="/login">Login</Link> : <Link to="/profile">Profile</Link>}
-            <Link to="/about">about</Link>
-            {auth ? <LogOut onClick={clickEvent}>Logout</LogOut> : <Redirect to="/"/>}
-        </nav>
+        <>
+            <nav className="flex flex-row w-auto sticky top-0 font-semibold px-4 py-1.5 h-12 text-gray-500 bg-white">
+                <Link className="px-6" to="/">Home</Link>
+                {status === 'hrd' ? <Link className="px-6" to="/postjob">Post Job</Link> : null}
+                <Link className="px-6" to="/jobs">List Job</Link>
+                {!auth ? <Link className="px-6" to="/register">Register</Link> : null}
+                {!auth ? <Link className="px-6" to="/login">Login</Link> : <Link to="/profile">Profile</Link>}
+                <Link className="px-6" to="/about">about</Link>
+                {auth ? <LogOut onClick={clickEvent}>Logout</LogOut> : <Redirect to="/"/>}
+            </nav>
+        </>
     )
 }
 
