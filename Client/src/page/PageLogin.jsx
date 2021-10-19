64bacd76d0000d1b4ae788569e64f206378fa74f
PC13-LAB TPM-1 <087762156347.ky@gmail.com>
2021-10-19 14:46:40 +0800

    page login regis

--- Client/src/page/PageLogin.jsx
+++ Client/src/page/PageLogin.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Login from "../components/Login";
-import style from "styled-components";
-
-const Body = style.div`
-height:36rem
-`;
 
 const PageLogin = () => {
   return (
-    <Body className="bg-gray-100">
+    <div className="bg-gray-100 h-screen">
       <Login />
-    </Body>
+    </div>
   );
 };
 
