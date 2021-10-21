2fadbc112465f589a890f2549dbc6395819c9a7a
TGB <087762156347.ky@gmail.com>
2021-10-21 11:21:09 +0800

    finall

--- Client/src/components/About.jsx
+++ Client/src/components/About.jsx
@@ -8,15 +8,27 @@ const About = () => {
       </div>
       <div className="flex justify-around px-6 items-center h-72 flex-wrap">
         <div className="flex flex-col items-center pb-10 sm:pb-0">
-          <img className=" rounded-full flex justify-center" src="https://images5.alphacoders.com/118/thumbbig-1182576.webp" width="200" alt="noProfile"/>
+          <img
+            className=" rounded-md flex justify-center img2"
+            src="https://firebasestorage.googleapis.com/v0/b/uploading-file-14552.appspot.com/o/WhatsApp%20Image%202021-10-20%20at%2020.55.37.jpeg?alt=media&token=02b26e0b-a049-44ac-a60d-3a4a87d19aa9"
+            alt="noProfile"
+          />
           <span className="text-center font-bold">Dedy Yoga</span>
         </div>
         <div className="flex flex-col items-center pb-10 sm:pb-0">
-        <img className=" rounded-full flex justify-center" src="https://images5.alphacoders.com/118/thumbbig-1182576.webp" width="200" alt="noProfile"/>
+          <img
+            className=" rounded-md flex justify-center img"
+            src="https://firebasestorage.googleapis.com/v0/b/uploading-file-14552.appspot.com/o/editPhoto_20210907_162456%20(1).jpg?alt=media&token=06463bc1-741f-40d9-9a11-3a5e3f43b970"
+            alt="noProfile"
+          />
           <span className="text-center font-bold">Ihsan</span>
         </div>
         <div className="flex flex-col items-center pb-10 sm:pb-0">
-        <img className=" rounded-full flex justify-center" src="https://images5.alphacoders.com/118/thumbbig-1182576.webp" width="200" alt="noProfile"/>
+          <img
+            className=" rounded-md flex justify-center img1"
+            src="https://firebasestorage.googleapis.com/v0/b/uploading-file-14552.appspot.com/o/WhatsApp%20Image%202021-10-20%20at%2020.55.13.jpeg?alt=media&token=3904f2b1-177f-4b42-9325-486c875168e4 "
+            alt="noProfile"
+          />
           <span className="text-center font-bold">Gilang</span>
         </div>
       </div>
